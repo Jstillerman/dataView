45419c561c326696de50553726ef40c162ebb038
Jstillerman <Jason@Jasons-MBP.hsd1.ma.comcast.net>
2015-03-09 15:46:52 -0400

    First Rift Commit
    Fix Raycasting…

--- Assets/raycast.cs
+++ Assets/raycast.cs
@@ -2,22 +2,29 @@
 using System.Collections;
 
 public class raycast : MonoBehaviour {
-	private CardboardHead head;
+	private OVRCameraRig head;
 	Color theColor = Color.black;
 	private GameObject tag;
+	Vector3 pos;
+	GameObject dot;
 
 	string value = "";
 	
 	void Start () {
-		head = Camera.main.GetComponent<StereoController>().Head;
+		//head = Camera.main.GetComponent<StereoController>().Head;
+		head = GameObject.Find ("OVRCameraRig").GetComponent<OVRCameraRig> ();
 		tag = GameObject.Find ("text");
+		dot = GameObject.Find ("Plane");
+
 		Debug.Log ("raycast enabled");
+		//Debug.DrawLine(, new Vector3(1, 0, 0), Color.red);
 		theColor = renderer.material.color;
 	}
 
 	void Update () {
 		RaycastHit hit;
-		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+		pos = dot.transform.position;
+		bool isLookedAt = GetComponent<Collider>().Raycast(head.camera.ViewportPointToRay(pos), out hit, Mathf.Infinity);
 		GetComponent<Renderer>().material.color = isLookedAt ? Color.green : theColor;
 		if (isLookedAt) {
 			tag.GetComponent<TextMesh>().text = value;
