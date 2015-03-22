c5ec46f9ce10d0e907e500a6590d8ab8d3cb328e
Jstillerman <js.skate.board@gmail.com>
2015-03-22 15:25:08 -0400

    Oculus Raycasting works

---
+++ Assets/centereyehack.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class centereyehack : MonoBehaviour {
+	public GameObject head;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("poopladoo");
+		head.transform.rotation = transform.rotation;
+		transform.rotation = new UnityEngine.Quaternion(0,0,0,0);
+
+	}
+}
--- Assets/raycast.cs
+++ Assets/raycast.cs
@@ -2,22 +2,24 @@
 using System.Collections;
 
 public class raycast : MonoBehaviour {
-	private CardboardHead head;
+	private GameObject head;
 	Color theColor = Color.black;
 	private GameObject tag;
 
 	string value = "";
-	
+
 	void Start () {
-		head = Camera.main.GetComponent<StereoController>().Head;
+		head = GameObject.Find("CenterEyeAnchor");
 		tag = GameObject.Find ("text");
 		Debug.Log ("raycast enabled");
 		theColor = GetComponent<Renderer>().material.color;
 	}
 
 	void Update () {
+		Ray theGaze = new Ray(head.transform.position, head.transform.forward);
+		Debug.DrawRay(head.transform.position, head.transform.forward*10, Color.white, 1.0f, true);
 		RaycastHit hit;
-		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+		bool isLookedAt = GetComponent<Collider>().Raycast(theGaze, out hit, Mathf.Infinity);
 		GetComponent<Renderer>().material.color = isLookedAt ? Color.green : theColor;
 		if (isLookedAt) {
 			tag.GetComponent<TextMesh>().text = value;
